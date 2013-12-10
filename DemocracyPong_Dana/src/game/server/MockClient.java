afc55bfce2310945e3fae49b6b99b64aed15bdda
unknown <ellingjp@uw.edu>
2013-12-09 20:46:28 -0800

    Rough tests for GamePlayServer

---
+++ DemocracyPong_Dana/src/game/server/MockClient.java
@@ -0,0 +1,90 @@
+package game.server;
+
+import game.entities.ClientState;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+
+public class MockClient implements Runnable {
+	static final int REG_TIMEOUT = 100;
+
+	private int userid;
+	private DatagramSocket ds;
+	private int team;
+	private ClientState cs;
+
+	public MockClient(String host, int port, int userid) {
+		this.userid = userid;
+		try {
+			ds = new DatagramSocket();
+			ds.connect(InetAddress.getByName(host), port);
+		} catch (SocketException | UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		cs = new ClientState(userid);
+		cs.yVote = 50;
+	}
+
+	@Override
+	public void run() {
+		getAndSendInitialInfo();
+		System.out.println("CLIENT " + userid + ": registered on team " + team);
+		
+		while (true) {
+			int plen = ClientState.getMaxSize();
+			byte[] buf = cs.toBytes();
+			DatagramPacket cp = new DatagramPacket(buf, plen);
+			try {
+				ds.send(cp);
+				Thread.sleep(50);
+			} catch (IOException | InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		// send client state
+		// receive client data
+	}
+
+	private void getAndSendInitialInfo(){
+		byte[] idBuf = new byte[4];
+		ByteBuffer buf = ByteBuffer.wrap(idBuf);
+		buf.putInt(userid);
+		DatagramPacket idPacket = new DatagramPacket(idBuf,idBuf.length);
+		
+		byte[] serverAckBuf = new byte[1];
+		DatagramPacket serverAckPacket = new DatagramPacket(serverAckBuf, serverAckBuf.length);
+		boolean receivedAck = false;
+		while(!receivedAck){
+			try {
+				ds.setSoTimeout(REG_TIMEOUT);
+				System.out.println("CLIENT " + userid + ": attempting to register");
+				ds.send(idPacket);
+				ds.receive(serverAckPacket); 
+				receivedAck = true;
+				byte indicator = serverAckBuf[0];
+				System.out.println("CLIENT " + userid + ": received ack: " + indicator);
+				if(indicator == 0 || indicator == 1){
+					team = indicator;
+				}else{
+					//server error, must exit
+					throw new RuntimeException("Server responded with code: "+indicator+", will not start.");
+				}
+			} catch (SocketTimeoutException e) {
+				continue;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		//throw new UnsupportedOperationException();
+	}
+}
---
+++ DemocracyPong_Dana/src/game/server/MockMain.java
@@ -0,0 +1,49 @@
+package game.server;
+
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import lobby.User;
+
+public class MockMain {
+	// create players list
+	// Start new GameServer
+	// spawn clients, which should have same userids as in player list
+
+	static final int NUM_PLAYERS = 50;
+	static Map<Integer, User> players = new HashMap<>();
+	static String HOST = "localhost";
+	static int PORT;
+
+	public static void main(String args[]) {
+
+		// populate players
+		for (int i = 0; i < NUM_PLAYERS; i++) {
+			int team = i % 2;
+			int yVote = (new Random()).nextInt(700);
+			players.put(i, new User(i, team, yVote));
+		}
+		
+		DatagramSocket ds = null;
+		try {
+			ds = new DatagramSocket(PORT);
+			PORT = ds.getLocalPort();
+			System.out.println(PORT);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		GameServer g = new GameSocketServer(players, ds);
+		g.start();
+		
+		// spawn MockClients
+		for (int i = 0; i < NUM_PLAYERS; i++) {
+			MockClient mc = new MockClient(HOST, PORT, i);
+			(new Thread(mc)).start();
+		}
+	}
+}
