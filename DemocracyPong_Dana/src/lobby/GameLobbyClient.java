03816e721640fe1eb81b08cf5786f8b5dd789963
Dana Van Aken <danava04@ericvh.cs.washington.edu>
2013-12-12 17:26:55 -0800

    added 'start'

--- DemocracyPong_Dana/src/lobby/GameLobbyClient.java
+++ DemocracyPong_Dana/src/lobby/GameLobbyClient.java
@@ -1,5 +1,10 @@
 package lobby;
 
+import game.client.GameClientConnection;
+import game.client.GameClientModel;
+import game.client.GameClientNetworkConnection;
+import game.client.GameClientWindow;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -16,11 +21,14 @@ import lobby.LobbyState.Room;
 public class GameLobbyClient extends Thread implements ConnectionBean {
 	private LobbyState lobbyState;
 	private ObjectOutputStream out;
+	private String hostname;
 	private Socket socket;
 	private int uid;
 	
 	public GameLobbyClient() {
 		lobbyState = new LobbyStateImpl(new ArrayList<Room>());
+		uid = -1;
+		hostname = null;
 	}
 	
 	public void run() {
@@ -39,7 +47,18 @@ public class GameLobbyClient extends Thread implements ConnectionBean {
 		    			uid = in.readInt();
 		    			break;
 		    		case START:
-		    			// do something!
+		    			int port = in.readInt();
+		    			System.out.printf("Received id= %d, port =%d\nTrying to connect now...\n",uid,port);
+		    			GameClientModel m = new GameClientModel();
+		    			GameClientWindow w = new GameClientWindow(m);
+		    			GameClientConnection c = new GameClientNetworkConnection(hostname, port, uid, w, m);
+		    			boolean success = c.connect();
+		    			if(!success){
+		    				System.err.println("Could not connect");
+		    			}else{
+		    				w.run();
+		    			}
+		    			break;
 		    		case UPDATE:
 		    			lobbyState = (LobbyState) in.readObject();
 		    			break;
@@ -100,7 +119,8 @@ public class GameLobbyClient extends Thread implements ConnectionBean {
 	}
 
 	@Override
-	public void connect(String hostname, int port) throws UnknownHostException, IOException{
+	public void connect(String hostname, int port) throws UnknownHostException, IOException {
+		this.hostname = hostname;
 		InetAddress addr = InetAddress.getByName(hostname);
 		socket = new Socket(addr, port);
 		
